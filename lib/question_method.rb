0ebbc95dd6eb9ea55d4395f3eacc7fce3ad9d8c8
ayano309 <ayatomato.k@gmail.com>
2022-04-29 15:33:48 +0000

    ?で終わるメソッド

---
+++ lib/question_method.rb
@@ -0,0 +1,20 @@
+#空文字列なら
+puts ''.empty?
+puts 'abc'.empty?
+
+#引数の文字列が含まれていれば
+puts 'watch'.include?('at')
+puts 'watch'.include?('in')
+
+#奇数なら
+puts 1.odd?
+puts 2.odd?
+
+#偶数なら
+puts 1.even?
+puts 2.even?
+
+
+#nil
+puts nil.nil?
+puts 'abc'.nil?
