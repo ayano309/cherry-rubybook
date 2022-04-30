12933f8b54b6d4fe75ad65f97ca943f33355aa8f
ayano309 <ayatomato.k@gmail.com>
2022-04-30 09:16:43 +0000

    　　hai

---
+++ lib/array.rb
@@ -0,0 +1,10 @@
+#divmodメソッドは商と余りを配列で返す
+puts 14.divmod(3)
+
+#戻り値を配列のまま受け取る
+quo_rem = 14.divmod(3)
+puts "商 = #{quo_rem[0]}, 余り = #{quo_rem[1]}"
+
+#多重代入で別々の変数として受け取る
+quotient, remainder = 14.divmod(3)
+puts "商 = #{quotient}, 余り = #{remainder}"
